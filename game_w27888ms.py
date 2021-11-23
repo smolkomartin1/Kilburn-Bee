f31fc5b1fd3185ff0754fe72d2a0bffee7064f60
w27888ms <martin.smolko@student.manchester.ac.uk>
2021-11-23 02:22:34 +0000

    Obstacles now bricks

--- game_w27888ms.py
+++ game_w27888ms.py
@@ -10,7 +10,7 @@ def collision(pipes):
     beeCoords = canvas.coords(bee)
     # checks just the first pipe, if pipe distance too short than add a for loop
     pipeTop = canvas.coords(pipes[0][0])
-    if (pipeTop[2] + 75) >= (beeCoords[0] + 55) >= pipeTop[0] and ((beeCoords[1] - 80) <= pipeTop[3] or (pipeTop[3] + 300) <= beeCoords[1]):
+    if (pipeTop[0] + 160) >= beeCoords[0] >= (pipeTop[0] - 48) and ((beeCoords[1] - 80) <= pipeTop[1] or (pipeTop[1] + 300) <= beeCoords[1]):
         return True
     return False
 
@@ -19,7 +19,7 @@ def start():
     speed = 0
     gravity = 0.3
     animation = 16
-    distanceBetweenPipes = 600
+    distanceBetweenPipes = 550
     sizePipeOpening = 300
     hit = False
     dead = False
@@ -37,17 +37,18 @@ def start():
                 canvas.move(pipes[i][0], -7, 0)
                 canvas.move(pipes[i][1], -7, 0)
             # If pipe out of screen then generate new one
-            if canvas.coords(pipes[0][0])[2] < 0:
-                pipes.pop(0)
-                newPipe = generatePipes(1, canvas.coords(pipes[len(pipes) - 1][0])[2] + distanceBetweenPipes - 100, sizePipeOpening)
+            if canvas.coords(pipes[0][0])[0] < -140:
+                canvas.delete(pipes.pop(0))
+                newPipe = generatePipes(1, canvas.coords(pipes[len(pipes) - 1][0])[0] + distanceBetweenPipes, sizePipeOpening)
                 pipes.append(newPipe)
             hit = collision(pipes)
         elif hit == True and dead == False:
             canvas.itemconfigure(bee, image=beeDead)
+            canvas.tag_raise(bee)
             dead = True
         canvas.move(bee, 0, speed)
         canvas.update()
-        sleep(0.001)
+        sleep(0.00001)
         if canvas.coords(bee)[1] > 900:
             canvas.itemconfigure(bee, image=beeDead)
             canvas.coords(bee, 250, 900)
@@ -59,27 +60,28 @@ def generatePipes(amount, distancePipes, sizeOpening):
     genPipes = []
     if amount == 1:
         opening = randint(50, 550)
-        genPipes.append(canvas.create_rectangle(distancePipes, 0, 100 + distancePipes, opening))
-        genPipes.append(canvas.create_rectangle(distancePipes, opening + sizeOpening, 100 + distancePipes, 900))
+        genPipes.append(canvas.create_image(distancePipes, opening, image=reverseObstacles[randint(0, 3)], anchor="sw"))
+        genPipes.append(canvas.create_image(distancePipes, opening + sizeOpening, image=obstacles[randint(0, 3)], anchor="nw"))
     else:
         for p in range(amount):
             pair = []
             opening = randint(50, 550)
-            pair.append(canvas.create_rectangle(1200 + distancePipes * p, 0, 1300 + distancePipes * p, opening))
-            pair.append(canvas.create_rectangle(1200 + distancePipes * p, opening + sizeOpening, 1300 + distancePipes * p, 900))
+            pair.append(canvas.create_image(1200 + distancePipes * p, opening, image=reverseObstacles[randint(0, 3)], anchor="sw"))
+            pair.append(canvas.create_image(1200 + distancePipes * p, opening + sizeOpening, image=obstacles[randint(0, 3)], anchor="nw"))
             genPipes.append(pair)
     return genPipes
-
 
 window = Tk()
 window.title("Kilburn Bee")
 window.geometry("1440x900")
 canvas = Canvas(window, width=1440, height=900)
 
-beeImages = [PhotoImage(file="bee_sprites/bee0.png"), PhotoImage(file="bee_sprites/bee1.png"), PhotoImage(file="bee_sprites/bee2.png"), PhotoImage(file="bee_sprites/bee3.png"), PhotoImage(file="bee_sprites/bee4.png"), PhotoImage(file="bee_sprites/bee5.png"), PhotoImage(file="bee_sprites/bee6.png"), PhotoImage(file="bee_sprites/bee7.png")]
-beeDead = PhotoImage(file="bee_sprites/dead.png")
-# kilburn = PhotoImage(file="kilburn5.png")
-# canvas.create_image(0, 0, image=kilburn, anchor="nw")
+beeImages = [PhotoImage(file="assets/bee0.png"), PhotoImage(file="assets/bee1.png"), PhotoImage(file="assets/bee2.png"), PhotoImage(file="assets/bee3.png"), PhotoImage(file="assets/bee4.png"), PhotoImage(file="assets/bee5.png"), PhotoImage(file="assets/bee6.png"), PhotoImage(file="assets/bee7.png")]
+beeDead = PhotoImage(file="assets/dead.png")
+obstacles = [PhotoImage(file="assets/obstacle0.png"), PhotoImage(file="assets/obstacle1.png"), PhotoImage(file="assets/obstacle2.png"), PhotoImage(file="assets/obstacle3.png")]
+reverseObstacles = [PhotoImage(file="assets/robstacle0.png"), PhotoImage(file="assets/robstacle1.png"), PhotoImage(file="assets/robstacle2.png"), PhotoImage(file="assets/robstacle3.png")]
+kilburn = PhotoImage(file="assets/kilburn.png")
+canvas.create_image(0, 0, image=kilburn, anchor="nw")
 canvas.pack()
 bee = canvas.create_image(250, 450, image=beeImages[7], anchor="s")
 jumped = True
